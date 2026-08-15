d367603a87ad8a09aab378dbb31ecff031caf806
nano034 <keisiri.f68@gmail.com>
2026-08-15 10:56:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -83,6 +83,15 @@ const SCRIPT_DATA = {
       { label: 'Open Rugger', code: 'loadstring(game:HttpGet("https://gist.githubusercontent.com/totohub-ex/3b5193e9ebcf99ab6460eab9f6dc7852/raw/gistfile1.txt"))()' },
     ],
   },
+   3: {
+    label: 'a',
+    cards: [
+      { label: 'Infinite Yield', code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()' },
+      { label: 'Ghost Hub', code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/tienkhanh11/spicy/refs/heads/main/GhostHub.lua"))()' },
+      { label: 'Fly Gui V3', code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()' },
+      { label: 'Aim Bot', code: 'loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Aimbot-Mobile-34677"))()' },
+    ],
+  },
 };
 function initScriptPage () {
   const select = document.getElementById('scriptSelect');
